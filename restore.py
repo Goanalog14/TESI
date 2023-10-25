0f5f3bc1be5136010123d4592f08871ba79ce3c3
Goanalog14 <124058067+Goanalog14@users.noreply.github.com>
2023-10-25 10:54:45 +0200

    Add files via upload

---
+++ restore.py
@@ -0,0 +1,21 @@
+import subprocess
+import virtualbox
+
+vbox = virtualbox.VirtualBox()
+vm_name = "kali-linux"
+
+vm = vbox.find_machine(vm_name)
+
+
+# Controlla che la vm è spenta
+if vm.state != 1:
+    print("non è spenta")
+    # allora spengi
+    subprocess.call(['VBoxManage','controlvm',vm_name,'poweroff'])
+else:
+    print("è spenta")
+
+
+#subprocess.call(['VBoxManage', 'snapshot', vm_name, 'restore', "vm_post_prova"])
+#subprocess.call(['VBoxManage', 'startvm', vm_name])
+
