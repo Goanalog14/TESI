8e997f4363760b8be0cd66dcf6925080cec08300
Goanalog14 <124058067+Goanalog14@users.noreply.github.com>
2023-12-20 12:17:36 -0500

    Add files via upload

---
+++ script_on_analyzer/pack.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+import subprocess
+import os
+
+virus = sys.argv[1]
+
+try:
+    subprocess.run(['/home/kali/Downloads/simplepack-master/pack', virus], check=True)
+    print("Programma 'pack' eseguito con successo.")
+except subprocess.CalledProcessError:
+    print("Errore durante l'esecuzione di 'pack'.")
+
+print("caricamento loader")
+curl_cmd = f"curl -X POST -F \"file=@loader\" http://192.168.56.101:8080/uploader"
+os.system(curl_cmd)
+
